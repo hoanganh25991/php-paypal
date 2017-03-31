393d6ecfa00ee355b5c858814e8698644f718d1e
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-03-31 09:24:41 +0700

    See paypal instance load login form, BUT only work for several country through BraintreeSDK

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 require __DIR__  . '/vendor/autoload.php';
+//require_once __DIR___ .'vendor/braintree/braintree_php/lib/Braintree.php';
+use Braintree\Gateway;
+
+
+
+
 
 $accessToken = file_get_contents(__DIR__ . '/access_token');
-
-$gateway = new Braintree_Gateway(array(
+$gateway = new Gateway(array(
     'accessToken' => $accessToken,
 ));
 
@@ -40,47 +45,57 @@ $clientToken = $gateway->clientToken()->generate();
 <!-- Use the components. We'll see usage instructions next. -->
 <!--suppress JSUnresolvedVariable -->
 <script>
-	// Fetch the button you are using to initiate the PayPal flow
-	var paypalButton = document.getElementById('paypal-button');
+    // Fetch the button you are using to initiate the PayPal flow
+    var paypalButton = document.getElementById('paypal-button');
 
 
 
-	// Create a Client component
-	braintree.client.create({
-//		authorization: 'TOKEN'
-		authorization: window.TOKEN
-	}, function (clientErr, clientInstance) {
-    // Create PayPal component
-    braintree.paypal.create({
-			client: clientInstance
-		}, function (err, paypalInstance) {
-        paypalButton.addEventListener('click', function () {
-            // Tokenize here!
-            paypalInstance.tokenize({
-					flow: 'checkout', // Required
-					amount: 10.00, // Required
-					currency: 'USD', // Required
-					locale: 'en_US',
-					enableShippingAddress: true,
-					shippingAddressEditable: false,
-					shippingAddressOverride: {
-                recipientName: 'Scruff McGruff',
-						line1: '1234 Main St.',
-						line2: 'Unit 1',
-						city: 'Chicago',
-						countryCode: 'US',
-						postalCode: '60652',
-						state: 'IL',
-						phone: '123.456.7890'
-					}
-				}, function (err, tokenizationPayload) {
-                // Tokenization complete
-                // Send tokenizationPayload.nonce to server
-                console.log(tokenizationPayload)
-				});
-			});
+    // Create a Client component
+    braintree.client.create({
+        //		authorization: 'TOKEN'
+        authorization: window.TOKEN
+    }, function(clientErr, clientInstance) {
+        // Create PayPal component
+        braintree.paypal.create({
+            client: clientInstance
+        }, function(err, paypalInstance) {
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            console.log('paypalInstance', paypalInstance);
+            paypalButton.addEventListener('click', function() {
+                // Tokenize here!
+                paypalInstance.tokenize({
+                    flow: 'checkout', // Required
+                    amount: 10.00, // Required
+                    currency: 'USD', // Required
+                    locale: 'en_US',
+                    enableShippingAddress: true,
+                    shippingAddressEditable: false,
+                    shippingAddressOverride: {
+                        recipientName: 'Scruff McGruff',
+                        line1: '1234 Main St.',
+                        line2: 'Unit 1',
+                        city: 'Chicago',
+                        countryCode: 'US',
+                        postalCode: '60652',
+                        state: 'IL',
+                        phone: '123.456.7890'
+                    }
+                }, function(err, tokenizationPayload) {
+                    // Tokenization complete
+                    // Send tokenizationPayload.nonce to server
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    console.log('tokenizationPayload', tokenizationPayload);
+                    console.log(tokenizationPayload)
+                });
+            });
+        });
     });
-	});
 </script>
 </body>
 </html>
