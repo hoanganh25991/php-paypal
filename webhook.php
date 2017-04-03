00e2dc3bf2208d139545ea1f174ca39a15a3fd3d
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-04-03 15:10:29 +0700

    Webhook hitted, try get info from req->body

--- webhook.php
+++ webhook.php
@@ -1,10 +1,13 @@
 <?php
 
-$post_json = json_encode($_POST);
+//$post_json = json_encode($_POST);
+
+$req_body  = file_get_contents('php://input');
 
 $webhook_log = fopen('webhook_log', 'a');
 
-fwrite($webhook_log, $post_json);
+//fwrite($webhook_log, $post_json);
+fwrite($webhook_log, $req_body);
 
 fclose($webhook_log);
 
